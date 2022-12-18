745b4eac0b18cb189277154e18eb1050429a7b6b
Christian Meng <cmeng@1-B-23.42heilbronn.de>
2022-12-18 21:29:42 +0100

    passed gnltester

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:13:38 by cmeng             #+#    #+#             */
-/*   Updated: 2022/12/16 17:23:11 by cmeng            ###   ########.fr       */
+/*   Updated: 2022/12/18 21:13:37 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_check_buf(char *t_buf);
+char	*ft_handle_buf(char *t_buf, int i);
 char	*ft_edit_buf(char *t_buf);
 int		ft_newline(char *buffer);
 
@@ -29,7 +29,6 @@ char	*get_next_line(int fd)
 	s_read = 1;
 	while (s_read != 0 && !ft_newline(t_buf))
 	{
-		// ft_bzero(buf, BUFFER_SIZE);
 		s_read = read(fd, buf, BUFFER_SIZE);
 		if (s_read < 1 && t_buf == NULL)
 			return (free(buf), free(t_buf), NULL);
@@ -41,44 +40,65 @@ char	*get_next_line(int fd)
 		}
 	}
 	free(buf);
-	out = ft_check_buf(t_buf);
+	out = ft_handle_buf(t_buf, 0);
 	if (out == NULL)
 		return (free(t_buf), NULL);
-	t_buf = ft_edit_buf(t_buf);
+	t_buf = ft_handle_buf(t_buf, 1);
 	if (t_buf == NULL && out == NULL)
 		return (NULL);
-	// if (s_read == 0)
-	// 	free (t_buf);
 	if (out != NULL)
 		return (out);
 	else
 		return (NULL);
 }
 
-char	*ft_check_buf(char *t_buf)
+// **********************************************
+//  x == 0 cut out until \n     x == 1 edit t_buf
+// **********************************************
+char	*ft_handle_buf(char *t_buf, int x)
 {
 	int		i;
 	char	*out;
 
 	i = 0;
+	if (t_buf[i] == '\0')
+		return (NULL);
 	while (t_buf[i] != '\n' && t_buf[i] != '\0')
 		i++;
-	out = ft_substr(t_buf, 0, i + 1);
+	if (x == 0)
+		out = ft_substr(t_buf, 0, i + 1);
+	else
+	{
+		out = ft_substr(t_buf, i + 1, ft_strlen(t_buf) - (i));
+		free(t_buf);
+	}
 	return (out);
 }
 
-char	*ft_edit_buf(char *t_buf)
-{
-	int		i;
-	char	*out;
+// char	*ft_check_buf(char *t_buf)
+// {
+// 	int		i;
+// 	char	*out;
 
-	i = 0;
-	while (t_buf[i] != '\n' && t_buf[i] != '\0')
-		i++;
-	out = ft_substr(t_buf, i + 1, ft_strlen(t_buf) - (i + 1));
-	free(t_buf);
-	return (out);
-}
+// 	i = 0;
+// 	while (t_buf[i] != '\n' && t_buf[i] != '\0')
+// 		i++;
+// 	out = ft_substr(t_buf, 0, i + 1);
+// 	return (out);
+// }
+
+// char	*ft_edit_buf(char *t_buf)
+// {
+// 	int		i;
+// 	char	*out;
+
+// 	i = 0;
+// 	while (t_buf[i] != '\n' && t_buf[i] != '\0')
+// 		i++;
+// 	out = ft_substr(t_buf, i + 1, ft_strlen(t_buf) - (i + 1));
+// 	free(t_buf);
+// 	return (out);
+// }
 
 int	ft_newline(char *t_buf)
 {
